8928525a8c3fd1be14f7d49c7b7f031f40558eb2
kareem250 <ahmadkireem6@gmail.com>
2025-08-04 02:03:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cars_chat/View/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+
+      //fix a bug
+    );
+  }
+}
